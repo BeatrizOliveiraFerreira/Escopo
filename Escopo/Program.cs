29ba721c376cb08628d6762563f60078f77d5c91
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-06 20:01:41 -0300

    Add project files.

---
+++ Escopo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+            if (preco > 100.00) {
+                desconto = preco * 0.1;
+            }
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
